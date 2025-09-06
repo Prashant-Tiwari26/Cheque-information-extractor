d895ea8a6ce3970e720a7cd735d9a1331158446d
Prashant Tiwari <prashant.st8@gmail.com>
2025-09-06 19:08:07 +0530

    ocr updates

--- src/models/ocr/ocr.py
+++ src/models/ocr/ocr.py
@@ -1,20 +1,11 @@
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 
-PROCESSOR_PRINTED = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
-OCR_MODEL_PRINTED = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
+PROCESSOR = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed', use_fast=True)
+OCR_MODEL = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
 
-PROCESSOR_HANDWRITTEN = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
-OCR_MODEL_HANDWRITTEN = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
-
-def ocr_image_printed(image):
-    pixel_values = PROCESSOR_PRINTED(images=image, return_tensors="pt").pixel_values
-    generated_ids = OCR_MODEL_PRINTED.generate(pixel_values)
-    generated_text = PROCESSOR_PRINTED.batch_decode(generated_ids, skip_special_tokens=True)[0]
+def ocr_image(image):
+    pixel_values = PROCESSOR(images=image, return_tensors="pt").pixel_values
+    generated_ids = OCR_MODEL.generate(pixel_values)
+    generated_text = PROCESSOR.batch_decode(generated_ids, skip_special_tokens=True)[0]
     return int(generated_text.replace(' ', ''))
-
-def ocr_image_handwritten(image):
-    pixel_values = PROCESSOR_HANDWRITTEN(images=image, return_tensors="pt").pixel_values
-    generated_ids = OCR_MODEL_HANDWRITTEN.generate(pixel_values)
-    generated_text = PROCESSOR_HANDWRITTEN.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    return generated_text
