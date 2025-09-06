e93d8f6afbf3fefd13d765cea7e4bb2012516d88
Prashant Tiwari <prashant.st8@gmail.com>
2025-09-06 19:20:35 +0530

    backend code

--- src/web/backend/celery_worker.py
+++ src/web/backend/celery_worker.py
@@ -6,7 +6,8 @@ os.environ.setdefault("FASTAPI_SETTINGS_MODULE", "src.web.backend.api")
 celery_app = celery.Celery(
     "Cheque Information Extractor",
     broker=os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0"),
-    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
+    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1"),
+    include=['src.web.backend.tasks']
 )
 
 celery_app.conf.update(
